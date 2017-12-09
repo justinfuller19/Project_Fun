83112aa0d98959c81900058de741ac27fff9bf28
justinfuller19 <34358942+justinfuller19@users.noreply.github.com>
2017-12-08 22:31:49 -0500

    Add files via upload

---
+++ XMLExporter/XMLExporter/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.InputTextBox = New System.Windows.Forms.TextBox()
+        Me.EnterButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(60, 74)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Customers"
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.Location = New System.Drawing.Point(126, 205)
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.InputTextBox.TabIndex = 2
+        '
+        'EnterButton
+        '
+        Me.EnterButton.Location = New System.Drawing.Point(439, 205)
+        Me.EnterButton.Name = "EnterButton"
+        Me.EnterButton.Size = New System.Drawing.Size(75, 23)
+        Me.EnterButton.TabIndex = 3
+        Me.EnterButton.Text = "Button1"
+        Me.EnterButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(844, 509)
+        Me.Controls.Add(Me.EnterButton)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "XML Conversion"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents InputTextBox As TextBox
+    Friend WithEvents EnterButton As Button
+End Class
---
+++ XMLExporter/XMLExporter/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Xml
+Imports Microsoft
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub EnterButton_Click(sender As Object, e As EventArgs) Handles EnterButton.Click
+        Dim Stuff As String
+        If Not InputTextBox.Text = "" Then
+            Stuff = InputTextBox.Text
+        End If
+        XML_Writer(Stuff)
+    End Sub
+    Private Sub XML_Writer(ByRef Stuff As String)
+        Dim thedate As String
+        thedate = DateString
+        If My.Computer.FileSystem.DirectoryExists("Desktop\") = False Then
+            My.Computer.FileSystem.CreateDirectory("Desktop\")
+        End If
+        Dim writer As XmlWriter = XmlWriter.Create("Desktop\" & thedate & ".xml")
+
+        With writer
+            If Stuff <> "" Then
+                .WriteStartElement("Customer")
+                .WriteString(Stuff)
+                .WriteEndElement()
+                .Close()
+            End If
+        End With
+
+
+    End Sub
+End Class
